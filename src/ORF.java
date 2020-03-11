d87e9da86fe57b5ac0090b5c70b4eb427e720ba0
armin <HJG.vanEldik@student.han.nl>
2020-03-11 10:00:40 +0100

    ORF class toegevoegt

--- src/ORF.java
+++ src/ORF.java
@@ -1,3 +1,65 @@
-public class ORF
-{
+public class ORF {
+    private String strand;
+    private String sequence;
+    private String label;
+    private int startPos;
+    private int endPos;
+
+    ORF(String sequence, String strand, String label, int startPos, int endPos) {
+        setSequence(sequence);
+        setStrand(strand);
+        setLabel(label);
+        setStartPos(startPos);
+        setEndPos(endPos);
+
+    }
+
+    public String getTranslation() {
+        return "top";
+    }
+
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getStrand() {
+        return strand;
+    }
+
+    public void setStrand(String strand) {
+        this.strand = strand;
+    }
+
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public int getStartPos() {
+        return startPos;
+    }
+
+    public void setStartPos(int startPos) {
+        this.startPos = startPos;
+    }
+
+    public int getEndPos() {
+        return endPos;
+    }
+
+    public void setEndPos(int endPos) {
+        this.endPos = endPos;
+    }
+
+
 }
+
