3532dfeb7bb09b04c435ce7c462216a358646ed0
armin <HJG.vanEldik@student.han.nl>
2020-03-11 10:38:15 +0100

    ORF class af

--- src/ORF.java
+++ src/ORF.java
@@ -1,3 +1,7 @@
+import java.util.Map;
+
+import static java.util.Map.entry;
+
 public class ORF {
     private String strand;
     private String sequence;
@@ -15,7 +19,29 @@ public class ORF {
     }
 
     public String getTranslation() {
-        return "top";
+        StringBuilder aminoAcid = new StringBuilder();
+        @SuppressWarnings("its long") Map<String, String> codToAa = Map.<String, String>ofEntries(
+                entry("ATA", "I"), entry("ATC", "I"), entry("ATT", "I"), entry("ATG", "M"),
+                entry("ACA", "T"), entry("ACC", "T"), entry("ACG", "T"), entry("ACT", "T"),
+                entry("AAC", "N"), entry("AAT", "N"), entry("AAA", "K"), entry("AAG", "K"),
+                entry("AGC", "S"), entry("AGT", "S"), entry("AGA", "R"), entry("AGG", "R"),
+                entry("CTA", "L"), entry("CTC", "L"), entry("CTG", "L"), entry("CTT", "L"),
+                entry("CCA", "P"), entry("CCC", "P"), entry("CCG", "P"), entry("CCT", "P"),
+                entry("CAC", "H"), entry("CAT", "H"), entry("CAA", "Q"), entry("CAG", "Q"),
+                entry("CGA", "R"), entry("CGC", "R"), entry("CGG", "R"), entry("CGT", "R"),
+                entry("GTA", "V"), entry("GTC", "V"), entry("GTG", "V"), entry("GTT", "V"),
+                entry("GCA", "A"), entry("GCC", "A"), entry("GCG", "A"), entry("GCT", "A"),
+                entry("GAC", "D"), entry("GAT", "D"), entry("GAA", "E"), entry("GAG", "E"),
+                entry("GGA", "G"), entry("GGC", "G"), entry("GGG", "G"), entry("GGT", "G"),
+                entry("TCA", "S"), entry("TCC", "S"), entry("TCG", "S"), entry("TCT", "S"),
+                entry("TTC", "F"), entry("TTT", "F"), entry("TTA", "L"), entry("TTG", "L"),
+                entry("TAC", "Y"), entry("TAT", "Y"), entry("TAA", "_"), entry("TAG", "_"),
+                entry("TGC", "C"), entry("TGT", "C"), entry("TGA", "_"), entry("TGG", "W")
+        );
+        for (int i = 0; i < this.sequence.length() / 3; i++) {
+            aminoAcid.append(codToAa.get(this.sequence.substring(i, i + 3)));
+        }
+        return aminoAcid.toString();
     }
 
 
@@ -59,7 +85,6 @@ public class ORF {
     public void setEndPos(int endPos) {
         this.endPos = endPos;
     }
-
 
 }
 
--- src/ORFPredictorGUI.java
+++ src/ORFPredictorGUI.java
@@ -1,3 +1,6 @@
-public class ORFPredictorGUI
-{
+public class ORFPredictorGUI {
+    public static void main(String[] args) {
+        ORF orf1 = new ORF("ATATACAGAGCAGCAGAGGCACGAGCGACAGGCAGCAGAGACGCAAGATTCGCTGGCSTCTSCGGCGCGAGTGATAGTAGAGGCGTCTCGTATATGCGCTC", "forward", "label", 1, 2);
+        System.out.println(orf1.getTranslation());
+    }
 }
