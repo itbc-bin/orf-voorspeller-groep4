72744f54238fb4848539d0f79bad96a11f406902
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2020-03-11 09:59:52 +0100

    test

--- src/BLASTResultsGUI.java
+++ src/BLASTResultsGUI.java
@@ -1,3 +1,30 @@
-public class BLASTResultsGUI
+import javax.swing.*;
+import java.sql.ResultSet;
+
+public class BLASTResultsGUI extends JFrame
 {
+    private JTable resultTable;
+    private ResultSet results;
+    private JButton refreshButton;
+
+    public static void main(String[] args)
+    {
+
+    }
+
+    public ResultSet getResults()
+    {
+        return null;
+    }
+
+    public void showResult()
+    {
+
+    }
+
+    public void refreshResults()
+    {
+
+    }
+
 }
