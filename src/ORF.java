2e62e03e851bcaa0042bf9a7a071096118187a90
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2020-03-17 15:19:09 +0100

    searching ORFs

--- src/ORF.java
+++ src/ORF.java
@@ -1,3 +1,89 @@
-public class ORF
-{
+import java.util.Map;
+import static java.util.Map.entry;
+
+public class ORF {
+    private String strand;
+    private String sequence;
+    private String label;
+    private int startPos;
+    private int endPos;
+
+    ORF(String sequence, String strand, String label, int startPos, int endPos) {
+        setSequence(sequence);
+        setStrand(strand);
+        setLabel(label);
+        setStartPos(startPos);
+        setEndPos(endPos);
+    }
+
+    public String getTranslation() {
+        StringBuilder aminoAcid = new StringBuilder();
+        Map<String, String> codToAa = Map.ofEntries(
+                entry("ATA", "I"), entry("ATC", "I"), entry("ATT", "I"), entry("ATG", "M"),
+                entry("ACA", "T"), entry("ACC", "T"), entry("ACG", "T"), entry("ACT", "T"),
+                entry("AAC", "N"), entry("AAT", "N"), entry("AAA", "K"), entry("AAG", "K"),
+                entry("AGC", "S"), entry("AGT", "S"), entry("AGA", "R"), entry("AGG", "R"),
+                entry("CTA", "L"), entry("CTC", "L"), entry("CTG", "L"), entry("CTT", "L"),
+                entry("CCA", "P"), entry("CCC", "P"), entry("CCG", "P"), entry("CCT", "P"),
+                entry("CAC", "H"), entry("CAT", "H"), entry("CAA", "Q"), entry("CAG", "Q"),
+                entry("CGA", "R"), entry("CGC", "R"), entry("CGG", "R"), entry("CGT", "R"),
+                entry("GTA", "V"), entry("GTC", "V"), entry("GTG", "V"), entry("GTT", "V"),
+                entry("GCA", "A"), entry("GCC", "A"), entry("GCG", "A"), entry("GCT", "A"),
+                entry("GAC", "D"), entry("GAT", "D"), entry("GAA", "E"), entry("GAG", "E"),
+                entry("GGA", "G"), entry("GGC", "G"), entry("GGG", "G"), entry("GGT", "G"),
+                entry("TCA", "S"), entry("TCC", "S"), entry("TCG", "S"), entry("TCT", "S"),
+                entry("TTC", "F"), entry("TTT", "F"), entry("TTA", "L"), entry("TTG", "L"),
+                entry("TAC", "Y"), entry("TAT", "Y"), entry("TAA", "_"), entry("TAG", "_"),
+                entry("TGC", "C"), entry("TGT", "C"), entry("TGA", "_"), entry("TGG", "W")
+        );
+        try {
+            for (int i = 0; i < this.sequence.length(); i += 3) {
+                aminoAcid.append(codToAa.get(this.sequence.substring(i, i + 3)));
+            }
+        } catch (StringIndexOutOfBoundsException ignored) {
+        }
+        return aminoAcid.toString();
+    }
+
+    public String getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(String sequence) {
+        this.sequence = sequence;
+    }
+
+    public String getStrand() {
+        return strand;
+    }
+
+    public void setStrand(String strand) {
+        this.strand = strand;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public int getStartPos() {
+        return startPos;
+    }
+
+    public void setStartPos(int startPos) {
+        this.startPos = startPos;
+    }
+
+    public int getEndPos() {
+        return endPos;
+    }
+
+    public void setEndPos(int endPos) {
+        this.endPos = endPos;
+    }
+
 }
+
--- src/ORFPredictorGUI.java
+++ src/ORFPredictorGUI.java
@@ -1,3 +1,66 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class ORFPredictorGUI
 {
+    private static ArrayList<String> stopCodons = new ArrayList<>(Arrays.asList("TAA", "TAG", "TGA"));
+    private static ArrayList<ORF> ORFs = new ArrayList<>();
+    private static int codonHeader = 1;
+
+
+
+    public static void main(String[] args)
+    {
+        String sequence = "ATGATGTAA";
+        String sequenceReversed = new StringBuilder(sequence).reverse().toString();
+        for (int i = 0; i < 3; i++)
+        {
+            findORF(i, sequence, "+");
+            findORF(i, sequenceReversed, "-");
+        }
+        ORFs.forEach((orf -> System.out.println(orf.getStrand())));
+
+    }
+
+    private static void findORF(int startPos, String sequence, String strand)
+    {
+        StringBuilder seq = new StringBuilder();
+        int endPos = 0;
+        // if sequence is not multiple of 3, get new sequence that starts at startPos and delete last part
+        String seqToLookIn = sequence.substring(startPos);
+        if (seqToLookIn.length() % 3 != 0)
+        {
+            int end = seqToLookIn.length() - seqToLookIn.length() % 3;
+            seqToLookIn = seqToLookIn.substring(0, end);
+        }
+
+        for (int i = 0; i < seqToLookIn.length(); i += 3)
+        {
+            String codon = seqToLookIn.substring(i, i + 3);
+            if (codon.equals("ATG"))
+            {
+                seq.append(codon);
+                for (int j = i; j < seqToLookIn.length(); j += 3)
+                {
+                    String cod = seqToLookIn.substring(j, j + 3);
+                    // ignore the same codon as in i loop
+                    if (j != i)
+                    {
+                        seq.append(stopCodons.contains(cod) ? "*" : cod);
+                    }
+                    if ((cod.equals("ATG") && seq.toString().contains("*")) || stopCodons.contains(cod)) {
+                        endPos = j;
+                        break;
+                    }
+                }
+                String label = ">ORF"+ codonHeader;
+                codonHeader++;
+                String ignoredStopCodonSequence = seq.substring(0, seq.length() - 1);
+                ORF orf = new ORF(ignoredStopCodonSequence, strand, label, i, endPos);
+                // ignore sequence that are either only ATG, or do not have a stop codon
+                if (seq.length() > 3 && seq.toString().endsWith("*")) ORFs.add(orf);
+                seq.setLength(0);
+            }
+        }
+    }
 }
