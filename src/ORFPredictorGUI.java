b722f73f1962828f46edf00dd15975590086bf82
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2020-03-18 10:31:56 +0100

    added minimal orf length, added ignore nested ors, refactor

--- src/ORFPredictorGUI.java
+++ src/ORFPredictorGUI.java
@@ -1,3 +1,7 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
 
@@ -6,59 +10,98 @@ public class ORFPredictorGUI
     private static ArrayList<String> stopCodons = new ArrayList<>(Arrays.asList("TAA", "TAG", "TGA"));
     private static ArrayList<ORF> ORFs = new ArrayList<>();
     private static int codonHeader = 1;
+    private static boolean ignoreNestedORFs = false;
+    private static int minimalORFLength = 3;
 
 
     public static void main(String[] args)
     {
-        String sequence = "ATGATGTAA";
+        File file = new File("/home/yarisvthiel/Documents/HAN/Course_7_Bio-informatica/Bio-informatica/Week4/SD.fa");
+        String sequence = readFile(file);
+//        String sequence = "ATGATGTAA";
         String sequenceReversed = new StringBuilder(sequence).reverse().toString();
         for (int i = 0; i < 3; i++)
         {
             findORF(i, sequence, "+");
             findORF(i, sequenceReversed, "-");
         }
-        ORFs.forEach(orf -> System.out.println(orf.getStrand()));
+        System.out.println(ORFs.size());
+//        ORFs.forEach(System.out::println);
 
     }
 
-    private static void findORF(int startPos, String sequence, String strand)
+    public static String readFile(File file)
+    {
+        StringBuilder seqTemp = new StringBuilder();
+        assert file != null;
+        try (BufferedReader br = new BufferedReader(new FileReader(file)))
+        {
+            String line;
+            while ((line = br.readLine()) != null)
+            {
+                if (!line.startsWith(">"))
+                {
+                    assert false;
+                    seqTemp.append(line.toUpperCase());
+                }
+            }
+        }
+        catch (IOException | NullPointerException e)
+        {
+            e.printStackTrace();
+        }
+        return seqTemp.toString();
+    }
+
+    public static String getCorrectSequence(int startPos, String sequence)
+    {
+        sequence = sequence.substring(startPos);
+        // if sequence is not multiple of 3, get new sequence that starts at startPos and delete last part
+        if (sequence.length() % 3 != 0)
+        {
+            int end = sequence.length() - sequence.length() % 3;
+            sequence = sequence.substring(0, end);
+        }
+        return sequence;
+    }
+
+    public static void findORF(int startPos, String sequence, String strand)
     {
         StringBuilder seq = new StringBuilder();
-        int endPos = 0;
-        // if sequence is not multiple of 3, get new sequence that starts at startPos and delete last part
-        String seqToLookIn = sequence.substring(startPos);
-        if (seqToLookIn.length() % 3 != 0)
+        int startOrfPos = 0;
+        int endOrfPos = 0;
+        sequence = getCorrectSequence(startPos, sequence);
+        for (int i = 0; i < sequence.length(); i += 3)
         {
-            int end = seqToLookIn.length() - seqToLookIn.length() % 3;
-            seqToLookIn = seqToLookIn.substring(0, end);
-        }
-
-        for (int i = 0; i < seqToLookIn.length(); i += 3)
-        {
-            String codon = seqToLookIn.substring(i, i + 3);
+            String codon = sequence.substring(i, i + 3);
             if (codon.equals("ATG"))
             {
                 seq.append(codon);
-                for (int j = i; j < seqToLookIn.length(); j += 3)
+                for (int j = i + 3; j < sequence.length(); j += 3)
                 {
-                    String cod = seqToLookIn.substring(j, j + 3);
+                    String cod = sequence.substring(j, j + 3);
                     // ignore the same codon as in i loop
                     if (j != i)
                     {
                         seq.append(stopCodons.contains(cod) ? "*" : cod);
                     }
-                    if ((cod.equals("ATG") && seq.toString().contains("*")) || stopCodons.contains(cod))
+                    if (stopCodons.contains(cod))
                     {
-                        endPos = j;
+                        startOrfPos = i;
+                        endOrfPos = j;
+                        if (ignoreNestedORFs) i = j - 3;
                         break;
                     }
                 }
                 String label = ">ORF" + codonHeader;
                 codonHeader++;
                 String ignoredStopCodonSequence = seq.substring(0, seq.length() - 1);
-                ORF orf = new ORF(ignoredStopCodonSequence, strand, label, i, endPos);
-                // ignore sequence that are either only ATG, or do not have a stop codon
-                if (seq.length() > 3 && seq.toString().endsWith("*")) ORFs.add(orf);
+
+                if (seq.length() > minimalORFLength && seq.toString().endsWith("*"))
+                {
+                    ORF orf = new ORF(ignoredStopCodonSequence, strand, label, startPos, startOrfPos, endOrfPos);
+                    ORFs.add(orf);
+                }
                 seq.setLength(0);
             }
         }
