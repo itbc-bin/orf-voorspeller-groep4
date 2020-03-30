659070425e5b9305eee5d5b929a482fbcf58a617
Yaris van Thiel <YJ.vanThiel@student.han.nl>
2020-03-30 21:31:15 +0200

    deleted files in old directory

--- pythonFiles/Linux/runPython.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-if [ ! -d env ]; then
-  python3 -m venv env
-  source pythonFiles/Linux/activate.sh
-  pip3 install -r pythonFiles/requirements.txt
-fi
-python3 pythonFiles/BLASTResultsParser.py $1 $2
-
--- pythonFiles/Windows/runPython.bat
+++
@@ -1,7 +0,0 @@
-IF NOT EXIST env(
-  py -m venv env
-  source pythonFiles/Linux/activate.sh
-  pip install -r pythonFiles/requirements.txt
-)
-python pythonFiles/BLASTResultsParser.py %1 %2
-
